e587e309b859b7587d9aa8f45683ee9043ca98b2
Niji Ano <fab-bertheol@hotmail.fr>
2022-01-17 20:28:47 +0100

    prepare encrypt module (password not work)

--- script.py
+++ script.py
@@ -61,17 +61,31 @@ def string_to_hex(text):
     return text.encode('utf-8').hex()
 
 
+def data_encode(password, f_data):
+    '''
+    Data encoder
+    '''
+    return str(hex(f_data ^ password))[2:]
+
+def data_decode(password, f_data):
+    '''
+    Data decoder
+    '''
+    return str(hex(f_data ^ password))[2:]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=str, required=True)
     parser.add_argument('-i', '--colorin', action='store_true')
     parser.add_argument('-o', '--colorout', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-p', '--password', type=str)
     args = parser.parse_args()
     colorfile = args.file
     colorin = args.colorin
     colorout = args.colorout
     verbose = args.verbose
+    password = args.password
 
     print(params_print('INFO'))
     print(r" ____      _          _____     _             ")
@@ -134,6 +148,15 @@ if __name__ == '__main__':
         COLOR_ARR = COLOR_ARR[16:]
         file_size = int(hex_to_string(''.join(file_size_hex)))
 
+        psw_hex = COLOR_ARR[:3]
+        COLOR_ARR = COLOR_ARR[3:]
+        psw = ''.join(psw_hex)
+
+        if psw == 'FFFFFF' and not password:
+            print(params_print('FAIL'))
+            print('Please set password' + params_print('END'))
+            sys.exit(1)
+
         COLOR_ARR_LEN = len(COLOR_ARR)
 
         if file_size != COLOR_ARR_LEN:
@@ -141,6 +164,14 @@ if __name__ == '__main__':
             print('[SIZE] Change ' + str(COLOR_ARR_LEN) + ' to ' + str(
                 file_size) + params_print('END'))
             COLOR_ARR = COLOR_ARR[:file_size]
+
+        # if password:
+        #     data_arr, n = ([], 3)
+        #     for index in range(0, len(COLOR_ARR), n):
+        #         f_data = "".join(COLOR_ARR[index : index + n])
+        #         data_arr.append(data_decode(0x12efee, int(f_data, 16)).rjust(6, '0'))
+
+        #     COLOR_ARR = data_arr
 
         bitout = open(colorfile.replace(FORMAT_OUTPUT, '') + '.decode' + extension, 'wb')
         bitout.write(binascii.a2b_hex(''.join(COLOR_ARR)))
@@ -171,11 +202,24 @@ if __name__ == '__main__':
         print('file_size:', file_size_hex, len(file_size_hex))
         print('extension:', extension_hex, len(extension_hex))
 
+        psw = '000000'
+        if password:
+            print('password : ***')
+            psw = 'FFFFFF'
+        else:
+            print('password : not set')
+
+        cnt = 0
         with open(colorfile, 'rb') as f:
-            hexdata = extension_hex + file_size_hex + f.read().hex()
+            hexdata = extension_hex + file_size_hex + psw + f.read().hex()
             data_arr, n = ([], 6)
             for index in range(0, len(hexdata), n):
-                data_arr.append(hexdata[index : index + n])
+                f_data = hexdata[index : index + n]
+                # if password and cnt > 9:
+                #     f_data = data_encode(0x12efee, int(f_data, 16))
+
+                data_arr.append(f_data)
+                cnt += 1
 
             img_wh = math.ceil(math.sqrt(len(data_arr)))
             print('img_dim  :', str(img_wh) + 'px2')
