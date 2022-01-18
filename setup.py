44c640aeb87c3b7a2997eaa94db42bd36d56298e
Niji Ano <fab-bertheol@hotmail.fr>
2022-01-18 18:33:31 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+Created by Anoniji
+Library made available under the terms of the license
+Attribution-NonCommercial-NoDerivs 3.0 Unported (CC BY-NC-ND 3.0)
+https://creativecommons.org/licenses/by-nc-nd/3.0/
+'''
+
+import sys
+from cx_Freeze import setup, Executable
+
+VERSION = '1.0.0.0'
+cible = Executable(script='script.py', base='Console', targetName="data_colors.exe")
+
+setup(
+    name='Data Colors',
+    version=VERSION,
+    author='Anoniji',
+    options={
+        'build_exe': {
+            'path': sys.path,
+            'includes': [],
+            'excludes': [
+                'numpy',
+                'matplotlib',
+                'tkinter',
+                'urllib3',
+                'zmq',
+                'urllib',
+                'requests',
+                'PyQt4',
+                'PyQt5',
+            ],
+            'packages': [],
+            'optimize': 2,
+            'silent': True,
+            'zip_include_packages': '*',
+            'zip_exclude_packages': '',
+            'include_msvcr': True,
+            'build_exe': './_build/',
+        },
+    },
+    executables=[cible],
+)
